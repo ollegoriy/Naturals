07dd9324a81431030b1c656561b799b10dce0f51
ollegoriy <oleggorin86@gmail.com>
2023-10-17 13:16:53 +0300

    Я хочу пиццы

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Note
+{
+    public string Title;
+    public string Description;
+    public DateTime Date;
+    public DateTime DueDate;
+
+    public Note(string title, string description, DateTime date)
+    {
+        Title = title;
+        Description = description;
+        Date = date;
+        DueDate = date;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<Note> notes = new List<Note>();
+    static int currentDateIndex = 0;
+    static int selectedNoteIndex = 0;
+
+    static void Main(string[] args)
+    {
+        InitializeNotes();
+
+        while (true)
+        {
+            Console.Clear();
+            DisplayMenu();
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            HandleKeyPress(keyInfo.Key);
+        }
+    }
+    static void InitializeNotes()
+    {
+        notes.Add(new Note("Заметка 1", "Сходить в магазин", new DateTime(2023, 10, 17)));
+        notes.Add(new Note("Заметка 2", "Сходить в магазин", new DateTime(2023, 10, 17)));
+        notes.Add(new Note("Заметка 1", "Забронировать номер в отеле в Турции", new DateTime(2023, 10, 18)));
+        notes.Add(new Note("Заметка 2", "Посетить музей", new DateTime(2023, 10, 18)));
+        notes.Add(new Note("Заметка 1", "Навестить родителей", new DateTime(2023, 10, 25)));
+    }
+    static void DisplayMenu()
+    {
+        Console.WriteLine("Ежедневник");
+
+        DateTime currentDate = notes[currentDateIndex].Date;
+        Console.WriteLine($"Дата: {currentDate.ToShortDateString()}\n");
+
+        for (int i = 0; i < notes.Count; i++)
+        {
+            if (notes[i].Date.Date == currentDate.Date)
+            {
+                string noteTitle = (i == selectedNoteIndex) ? $"> {notes[i].Title}" : notes[i].Title;
+                Console.WriteLine(noteTitle);
+            }
+        }
+    }
+    static void HandleKeyPress(ConsoleKey key)
+    {
+        switch (key)
+        {
+            case ConsoleKey.LeftArrow:
+                ChangeDate(-1);
+                break;
+
+            case ConsoleKey.RightArrow:
+                ChangeDate(1);
+                break;
+
+            case ConsoleKey.UpArrow:
+                ChangeNoteSelection(-1);
+                break;
+
+            case ConsoleKey.DownArrow:
+                ChangeNoteSelection(1);
+                break;
+
+            case ConsoleKey.Enter:
+                DisplayNoteDetails();
+                break;
+        }
+    }
+    static void ChangeDate(int increment)
+    {
+        currentDateIndex += increment;
+        if (currentDateIndex < 0)
+            currentDateIndex = notes.Count - 1;
+        else if (currentDateIndex >= notes.Count)
+            currentDateIndex = 0;
+
+        selectedNoteIndex = 0;
+    }
+
+    static void ChangeNoteSelection(int increment)
+    {
+        selectedNoteIndex += increment;
+        if (selectedNoteIndex < 0)
+            selectedNoteIndex = notes.Count - 1;
+        else if (selectedNoteIndex >= notes.Count)
+            selectedNoteIndex = 0;
+    }
+    static void DisplayNoteDetails()
+    {
+        Console.Clear();
+        Console.WriteLine("Полная информация о заметке\n");
+
+        Note selectedNote = notes[selectedNoteIndex];
+        Console.WriteLine($"Название: {selectedNote.Title}");
+        Console.WriteLine($"Описание: {selectedNote.Description}");
+        Console.WriteLine($"Дата: {selectedNote.Date.ToShortDateString()}");
+        Console.WriteLine($"Когда выполнить: {selectedNote.DueDate.ToShortDateString()}");
+        Console.WriteLine("\nНажмите Enter для продолжения...");
+        Console.ReadLine();
+    }
+
+}
